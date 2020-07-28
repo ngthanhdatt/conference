25f2faaf008c3ad56593ab2905c406ed065e1095
thanhdatdat <46674716+thanhdatdat@users.noreply.github.com>
2020-07-28 17:15:30 +0700

    add sql file

--- database/baitap3.sql
+++ database/baitap3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 03, 2020 lúc 05:20 PM
+-- Thời gian đã tạo: Th7 28, 2020 lúc 12:13 PM
 -- Phiên bản máy phục vụ: 10.4.11-MariaDB
 -- Phiên bản PHP: 7.4.4
 
@@ -41,9 +41,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`ID`, `username`, `password`, `name`, `phone`, `email`) VALUES
-(1, 'thanhdatdat', 'admin', 'thanh dat', '0961003483', 'thanhdat12a2ntt99@gmail.com'),
-(3, 'dat', '12345', 'thanhdatnguyen', '123545', 'dat@gmail.com'),
-(6, 'thu uyen', '123455', 'luongthuuyen', '1233432423', 'uyen@gmail.com');
+(1, 'thanhdatdat', '1234', 'thanh dat', '0961003483', 'thanhdat12a2ntt99@gmail.com'),
+(3, 'dat', 'dat123', 'thanhdatnguyen', '0976549998', 'dat@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -64,8 +63,11 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`ID`, `name`, `phone`, `email`, `ticker`) VALUES
-(2, 'luongthuuyen', '1233432423', 'uyen@gmail.com', '60.00'),
-(8, 'Đạt', '0961003483', 'thanh@gmail.com', '60.00');
+(8, 'nguyễn thành đạt', '0961003483', 'thanh@gmail.com', '60.00'),
+(26, 'nguyen thanh dat dat', '0987654', 'datptit.99@gmail.com', '60.00'),
+(30, 'nguyen tuan nam', '987654345678', 'nam@gmail.com', '50.00'),
+(34, 'lê thái sơn', '1324343545', 'sonson@gmail.com', '70.00'),
+(35, 'nguyễn thành đạt', '0961003483', 'thanhdat12a2ntt99@gmail.com', '50.00');
 
 -- --------------------------------------------------------
 
@@ -79,16 +81,18 @@ CREATE TABLE `speaker` (
   `phone` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `professional` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `CCCC` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+  `CCCC` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `hotel` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `speaker`
 --
 
-INSERT INTO `speaker` (`ID`, `name`, `phone`, `email`, `professional`, `CCCC`) VALUES
-(1, 'Đạt', '0961003483', 'thanh@gmail.com', 'machine learning', '1234545'),
-(3, 'luongthuuyen', '1233432423', 'uyen@gmail.com', 'deep learning', '1234567890');
+INSERT INTO `speaker` (`ID`, `name`, `phone`, `email`, `professional`, `CCCC`, `hotel`) VALUES
+(4, 'nguyễn tuấn nam', '12325345', 'nam@gmail.com', 'deep learning', '1234545', 'phòng 507 khách sạn JW marriot'),
+(5, 'trần lê minh', '1321443435', 'minh@gmail.com', 'AI', '4234423423', 'phòng 707 khách sạn metropole'),
+(6, 'nguyễn ngọc hải', '09876543', 'hai@gmail.com', 'AI, Machine learning', '099877', 'metropolis');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -120,19 +124,19 @@ ALTER TABLE `speaker`
 -- AUTO_INCREMENT cho bảng `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT cho bảng `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT cho bảng `speaker`
 --
 ALTER TABLE `speaker`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
